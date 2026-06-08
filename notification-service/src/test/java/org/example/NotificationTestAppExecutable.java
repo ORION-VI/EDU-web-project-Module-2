4f82100edbc14bd2cdda105ee3d9cee03dbffcaf
ORION-VI <orion.digiworks@gmail.com>
2026-06-08 21:55:44 +0300

    Коммит #1. 1. Реализован паттерн service discovery через отдельный модуль service-discovery. 2. Изменен подход к логгированию (один лог файл на модуль, без аппенда). 3. Проперти файлы заменены yaml, для большей репрезентативности.

--- notification-service/src/test/java/org/example/NotificationTestAppExecutable.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class NotificationTestAppExecutable {
-    public static void main(String[] args) {
-    }
-}
