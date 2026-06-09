2e56699fd7d669adc5d7a265e2b8bcae8fbd8ae8
ORION-VI <orion.digiworks@gmail.com>
2026-06-09 21:31:48 +0300

    Коммит #2. 1. Реализован паттерн API Gateway. 2. Сконфигурированы yml файлы модулей для работы с Gateway API. 3. Внесены изменения в pom файлы модулей (добавлены зависимости для Gateway API, добавлен пак зависимостей для Spring Cloud в root pom во избежание конфликтов версий).

---
+++ api-gateway/src/main/java/org/example/ApiGatewayAppExecutable.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayAppExecutable {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayAppExecutable.class, args);
+    }
+}
